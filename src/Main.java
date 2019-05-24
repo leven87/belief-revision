1069ffbabedf1cf9618f21c3081e22ddfeb85978
Allan Laursen <s160524@student.dtu.dk>
2019-05-24 19:29:34 +0200

    Updated main again, now down to one error

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,16 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
 
-	public static void main (String [] args) {
+	public static void main (String [] args) throws IOException {
 		KnowledgeBase kb = new KnowledgeBase();
-		BufferedReader in = new BufferedReader(new FileReader("input.txt"));
+		BufferedReader in;
+		String line;
+		in = new BufferedReader(new FileReader("input.txt"));
+
 
 
 		// Initialize the Knowledge base. Print initial belief set.
@@ -13,8 +18,9 @@ public class Main {
 		kb.printKnowledgeBase();
 
 		// Update the knowledge base and print new belief set.
-		while((Sentence line = (in.readLine(), true)) != null){
-			KB.updateKnowledgeBase(line);
+		while((line = in.readLine()) != null) {
+			Sentence input = (line, true);
+			kb.updateKnowledgeBase(input);
 		}
 
 		kb.printKnowledgeBase();
