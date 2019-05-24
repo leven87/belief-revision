7792733c6b13f80ea20f829f30d19387a5d02bb3
Allan Laursen <s160524@student.dtu.dk>
2019-05-24 16:56:06 +0200

    And hopefully it works this time.

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class Main {
+
+    public static void main (String [] args) {
+		KnowledgeBase kb = new KnowledgeBase();
+		BufferedReader in = new BufferedReader(new FileReader("C:/input.txt"));
+		List<String> lines = new ArrayList<String>();
+
+		while((String line = in.readLine()) != null){
+
+			lines.add(line);
+		}
+		kb.initKnowledgeBase(lines.toArray());
+		kb.printBelief();
+    }
+}
