d9f6ae860da623fa44ac827219d0d48035e86ce5
Allan Laursen <s160524@student.dtu.dk>
2019-05-24 17:43:36 +0200

    Updated Main to take input from file.

--- src/Main.java
+++ src/Main.java
@@ -5,28 +5,20 @@ import java.util.List;
 public class Main {
 
     public static void main (String [] args) {
-<<<<<<< HEAD
 		KnowledgeBase kb = new KnowledgeBase();
-		BufferedReader in = new BufferedReader(new FileReader("C:/input.txt"));
-		List<String> lines = new ArrayList<String>();
+		BufferedReader in = new BufferedReader(new FileReader("input.txt"));
 
-		while((String line = in.readLine()) != null){
 
-			lines.add(line);
+		// Initialize the Knowledge base. Print initial belief set.
+		kb.initKnowledgeBase();
+		kb.printKnowledgeBase();
+		
+		// Update the knowledge base and print new belief set.
+		while((Sentence line = in.readLine()) != null){
+			KB.updateKnowledgeBase(line);
 		}
-		kb.initKnowledgeBase(lines.toArray());
-		kb.printBelief();
-=======
-    	KnowledgeBase KB = new KnowledgeBase();
-    	
-    	KB.initKnowledgeBase();
-    	
-        System.out.println("The initial knowledge base are:");
-        KB.printKnowledgeBase();    	
-    	Sentence input = new Sentence("¬q", true);
-    	KB.updateKnowledgeBase(input);
-        System.out.println("The revised knowledge base are:");
-        KB.printKnowledgeBase();     	
->>>>>>> 71671091679f47a1652fbcaf41ae6fd24fa60382
+
+		kb.printKnowledgeBase();
+
     }
 }
