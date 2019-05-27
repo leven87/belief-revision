7c5f6941a8de823e4ac0922bd9c68484e872bb65
Allan Laursen <s160524@student.dtu.dk>
2019-05-27 11:45:49 +0200

    Copied initial KB from KnowledgeBase to initialKB.txt

--- src/Main.java
+++ src/Main.java
@@ -6,21 +6,31 @@ public class Main {
 
 	public static void main (String [] args) throws IOException {
 		KnowledgeBase kb = new KnowledgeBase();
-		BufferedReader in;
+		BufferedReader input,initial;
 		String line;
-		in = new BufferedReader(new FileReader("input.txt"));
+		Sentence pass;
+		initial = new BufferedReader(new FileReader("input.txt"));
+		input = new BufferedReader(new FileReader("input.txt"));
 
 
 
-		// Initialize the Knowledge base. Print initial belief set.
+		// Initialize the Knowledge base. Input and print initial belief set.
 		kb.initKnowledgeBase();
+
+		while((line = initial.readLine()) != null) {
+			pass = new Sentence (line, true);
+			kb.updateKnowledgeBase(pass);
+		}
+
 		System.out.println("The knowledge base before new facts have been added: ");
 		kb.printKnowledgeBase();
 
+
+
 		// Update the knowledge base and print new belief set.
-		while((line = in.readLine()) != null) {
-			Sentence input = new Sentence (line, true);
-			kb.updateKnowledgeBase(input);
+		while((line = input.readLine()) != null) {
+			pass = new Sentence (line, true);
+			kb.updateKnowledgeBase(pass);
 		}
 		System.out.println(" ");
 		System.out.println("The knowledge base after new facts have been added: ");
