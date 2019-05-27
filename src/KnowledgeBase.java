ce9c86f0da8a9b4bd740d7a5daf3cbcd2f2cdf70
leven87 <176478015@qq.com>
2019-05-27 20:22:33 +0200

    update

--- src/KnowledgeBase.java
+++ src/KnowledgeBase.java
@@ -50,21 +50,6 @@ public class KnowledgeBase {
         
     }
       
-    /*
-     * We assume KB have limited atomic formulas, i.e. only 5(p,q,r,s,t).
-     * then, we can build truth table for each sentence in KB and input. Also, we set all sentence state false at first. 
-     * We just validate the sentence with state false by following rules:
-     * 
-     * 1.the truth table of sentence is out that of input, return 0 (false);
-     * 2. others return 1.
-     *
-     * 0 means we delete the sentence from KB; then if there are still sentence in KB, we choose the highest priority one, 
-     * set it state true and join it in the input set, i.e, makes a conjunction between its truth table and the existing 
-     * input truth table. Then we repeat the validate process as before, until there is no sentence with false in KB. And 
-     * the true sentence in KB and the input would be the new KB.  
-     * 
-     */
-
 
 	/*
 	// Uncomment this section to use the validator
@@ -87,6 +72,21 @@ public class KnowledgeBase {
 		Boolean result = validate.visit(parseTree);
 	}*/
 
+    
+    /*
+     * We assume KB have limited atomic formulas, i.e. only 5(p,q,r,s,t).
+     * then, we can build truth table for each sentence in KB and input. Also, we set all sentence state false at first. 
+     * We just validate the sentence with state false by following rules:
+     * 
+     * 1.the truth table of sentence is out that of input, return 0 (false);
+     * 2. others return 1.
+     *
+     * 0 means we delete the sentence from KB; then if there are still sentence in KB, we choose the highest priority one, 
+     * set it state true and join it in the input set, i.e, makes a conjunction between its truth table and the existing 
+     * input truth table. Then we repeat the validate process as before, until there is no sentence with false in KB. And 
+     * the true sentence in KB and the input would be the new KB.  
+     * 
+     */    
      public void updateKnowledgeBase(Sentence input) {
     	 
     	List<Sentence> to_revise_sentences = this.sentences;
@@ -103,7 +103,7 @@ public class KnowledgeBase {
 //    	true_sentences.add(input);
     	Map<String, Boolean> true_sententces_truth_table = input.getCompleteTruthtable();
     	
-    	System.out.println("here1");
+    	//System.out.println("here1");
     	while(depends_sentences_length > 0) {
     		ArrayList delete_sentence_num_list = new ArrayList(); 
     		for(int i=0;i<to_revise_sentences.size(); i++) { 
@@ -150,7 +150,7 @@ public class KnowledgeBase {
     		}
     	}
 
-    	System.out.println("here");
+    	//System.out.println("here");
     	
     	to_revise_sentences = this.removeDuplicate(to_revise_sentences,input);
     	to_revise_sentences.add(0,input);
@@ -220,201 +220,4 @@ public class KnowledgeBase {
         return list;
     }
     
-    /**
-     * Updating the current knowledge bases and provides each Validator with a new priority value
-     *  - newest information gets highest priority.
-     *  
-     * I assume just have ∨,¬, and sentences are in Horn clause. 
-     * Firstly,I check each sentences in KB with input, if consistent, label it true, 
-     * if not consistent,delete it, otherwise label it false(can't decided yet). 
-     * Then, I choose the highest priority(lowest index) labeled false sentence and label it true.
-     * Then, check all the labeled false sentences with input and this sentence , 
-     * then we also can turn some false(can't decided yet) sentence true or delete it. 
-     * We go on and on, finally, we just have true sentences. and these with input are new KB.
-     * 
-     * @param input Validator to update the knowledge base with
-     */
-    public int updateKnowledgeBase2 (Sentence input) {
-    	
-    	List<Sentence> to_revise_sentences = this.sentences;
-    	
-    	for (Sentence s : to_revise_sentences) {
-    		s.setState(false); // firstly,set all sentences false, we don't believe everything in the knowledge base
-    	}
-    	
-    	int depends_sentences_length = to_revise_sentences.size();
-    	ArrayList<Sentence> true_sentences = new ArrayList<Sentence>(); 
-    	true_sentences.add(input);
-    	
-    	System.out.println("here");
-    	while(depends_sentences_length > 0) {
-    		ArrayList delete_sentence_num_list = new ArrayList(); 
-    		for(int i=0;i<to_revise_sentences.size(); i++) { 
-    			Sentence s  = to_revise_sentences.get(i);
-	    	//for (Sentence s : to_revise_sentences) {
-	    		if(s.isState() == true) {
-	    			continue;
-	    		}
-	    		
-	    		String[] validate_result  = this.validate (s,"∧",true_sentences);
-	    		
-	    		
-	    		if(validate_result[0] == "0") {// if false, delete the sentence
-	    			//to_revise_sentences.remove(s);
-	    			delete_sentence_num_list.add(i);
-	    			depends_sentences_length --;
-	    		}
-	    		else if(validate_result[0] == "1") {// if true, keep the sentence
-	    			s.setState(true);
-	    			depends_sentences_length --;
-	    		}
-	    		else if(validate_result[0] == "2") {// if depends, set the sentence state false, and update the sentence to simplified version
-	    			System.out.println(validate_result[1]);
-	    			//s.setName();
-	    		}    		
-	    		System.out.println(validate_result[0]);
-	    		
-	    		//System.out.println(s.getName());
-	    	} 
-    		
-    		for(int i=0; i<delete_sentence_num_list.size();i++) {
-    			to_revise_sentences.remove((int)delete_sentence_num_list.get(i));
-    		}
-    		
-    		if(depends_sentences_length <= 0) {
-    			break;
-    		}
-    		
-    		//set the highest priority sentence which is false to be true
-    		//the lowest index sentenece has the highest priority
-    		for(int i=0;i<to_revise_sentences.size();i++) {
-    			Sentence s  = to_revise_sentences.get(i);
-    			if(s.isState() == false) {
-    				to_revise_sentences.get(i).setState(true);
-    				true_sentences.add(to_revise_sentences.get(i));
-    				depends_sentences_length--;
-    				break;
-    			}
-    		}
-    	}
-
-    	System.out.println("here");
-    	
-    	//to_revise_sentences.add(input);
-    	to_revise_sentences = this.removeDuplicate(to_revise_sentences,input);
-    	to_revise_sentences.add(0,input);
-    	
-    	this.sentences = to_revise_sentences;
-    	return 1;
-    	//Sentence s = sentences.get(0); 
-    	//Validator v = new Validator(s,"∧",input);  
-    }
-    
-    
-    private String[] validate (Sentence p, String notation, ArrayList<Sentence> q_arr) {
-    	
-    	String[] result = new String[2];
-        Map<String,Boolean> p_atoms_table = new HashMap<String,Boolean>(); 
-        Map<String,Boolean> q_atoms_table = new HashMap<String,Boolean>();
-        String[] atoms;
-        //ArrayList<String> atoms_table = new ArrayList<>();
-        
-        String p_name = p.getName();
-        atoms = p_name.split("∨");
-       
-        for(int i=0; i< atoms.length;i++) {
-        	if(atoms[i].charAt(0)=='¬') {
-        		p_atoms_table.put(atoms[i].substring(1), false); 
-        		//atoms_table.add(atoms[i].substring(1));
-        	}else {
-        		p_atoms_table.put(atoms[i], true); 
-        	}
-        }
-
-        
-        ArrayList<String> delete_p_atoms_list = new ArrayList<String>(); 
-        int inconsistent_count =0;
-        //pick each atom in p, validate it in q 
-        for (String p_atom : p_atoms_table.keySet()) {
-        	
-        	boolean consistent = false;
-        	boolean inconsistent = false;
-        	Boolean p_atom_value = p_atoms_table.get(p_atom);
-	        	
-	    	for(int j=0;j<q_arr.size(); j++) {//initialize, turn string to atom formulas
-	    		Sentence q = q_arr.get(j);
-	            String q_name = q.getName();
-	            
-	//                if(q_name.contains("∨")){
-	//                	atoms = q_name.split("∨");  
-	//                }
-	//                else{
-	//                	atoms = q_name.split("∧");
-	//                }
-	            atoms = q_name.split("∨");                
-	            for(int i=0; i< atoms.length;i++) {
-	            	if(atoms[i].charAt(0)=='¬') {
-	            		q_atoms_table.put(atoms[i].substring(1), false);
-	            	}else {
-	            		q_atoms_table.put(atoms[i], true); 
-	            	}
-	            }
-	            
-	            
-	            for (String q_atom : q_atoms_table.keySet()) {
-   
-	            	if(p_atom.equals(q_atom)) {
-	            		Boolean q_atom_value = q_atoms_table.get(q_atom);
-	        			if(p_atom_value != q_atom_value) {
-	        				delete_p_atoms_list.add(p_atom);
-	        				inconsistent_count ++;
-	        				inconsistent = true;
-	        				break;
-	        			}else {
-	        				consistent = true;
-	        				break;
-	        			}
-	            	}else {
-	            		continue;
-	            	}                    
-	        	} 
-	            
-	            if(consistent == true || inconsistent == true) {
-	            	break;
-	            }
-	
-	        }        	
-        }
-
-   
-		for(int i=0; i<delete_p_atoms_list.size();i++) {
-			p_atoms_table.remove(delete_p_atoms_list.get(i));
-		}          
-        
-        
-        if(p_atoms_table.size() == 0) {
-        	result[0] = "0";//false
-        }else if(inconsistent_count > 0) {
-        	p_name = "";	
-        	for (String p_atom : p_atoms_table.keySet()) {
-        		 
-        		if(p_atoms_table.get(p_atom) == true ) {
-        			p_name +=  p_atom + "∨";
-        		}else {
-        			p_name += "¬" + p_atom + "∨";
-        		}
-        	}
-        	if(p_name.charAt(p_name.length()-1)=='∨') {
-        		p_name = p_name.substring(0, p_name.length()-1);
-        	}      
-        	result[0] = "2";
-        	result[1] = p_name;//depends
-        	//System.out.println("here");
-        }else {
-        	result[0] = "1";//true
-        }
-
-        return result;
-    }
-        
 }
